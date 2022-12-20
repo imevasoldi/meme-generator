5e5b3d8ff7cfe27bd0757d96dc7f3d8286b8005b
imevasoldi <imevasoldi@gmail.com>
2022-12-20 12:12:01 -0600

    add header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+export const Header = () => (
+    <div className="header--">
+        <div className="header--logo">
+            <img src="Troll Face.png"/>
+            <h3>Meme Generator</h3>
+        </div>
+        <h4>React Project</h4>
+    </div>
+)
