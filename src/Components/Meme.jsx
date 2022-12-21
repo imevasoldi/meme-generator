09ae7e832d9e3b87fe2f11d6e283298107a34952
imevasoldi <imevasoldi@gmail.com>
2022-12-20 18:34:07 -0600

    getting a random image and avoiding components getting outside containers

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,11 +1,23 @@
+import memesData from "../memesData"
+
 function Meme(){
-    return <form>
-        <div className="inputs--">
-            <input placeholder="Top text"/>
-            <input placeholder="Bottom text"/>
+    const data = memesData
+    // console.log(data.data.memes)
+    const randomImageIndex = Math.floor(Math.random() * 99)
+    console.log(randomImageIndex)
+    return (
+        <div>
+        <div className="form">
+            <div className="inputs--">
+                <input placeholder="Top text"/>
+                <input placeholder="Bottom text"/>
+            </div>
+            <button>Get a new meme image 🖼</button>
         </div>
-        <button>Get a new meme image 🖼</button>
-    </form>
+        <img className="meme--image" src={memesData.data.memes[randomImageIndex].url}/>
+        </div>
+        )
+        
 }
 
 export default Meme
