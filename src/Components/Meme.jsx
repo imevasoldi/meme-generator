96a4679f854f9179652020c8e045974cf4412afd
imevasoldi <imevasoldi@gmail.com>
2022-12-27 15:44:45 -0600

    adding css for top and bottom text

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,15 +1,44 @@
 import { useState } from "react"
 import memesData from "../memesData"
+/**
+     * Challenge: Update our state to save the meme-related
+     * data as an object called `meme`. It should have the
+     * following 3 properties:
+     * topText, bottomText, randomImage.
+     * 
+     * The 2 text states can default to empty strings for now,
+     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+     * 
+     * Next, create a new state variable called `allMemeImages`
+     * which will default to `memesData`, which we imported above
+     * 
+     * Lastly, update the `getMemeImage` function and the markup 
+     * to reflect our newly reformed state object and array in the
+     * correct way.
+     */
 
 function Meme(){
-    const [memeImage, setMemeImage] = useState("")
-    const data = memesData
+    const [meme, setMeme] = useState(
+            {
+                topText: "",
+                topBottom: "",
+                randomImage: "http://i.imgflip.com/1bij.jpg",
+            }
+        )
+    const [allMemeImages, setAllMemesImages] = useState(memesData)
+    // const data = memesData
     
     const getMemeImage = () => {
         const randomImageIndex = Math.floor(Math.random() * 99)
         console.log(randomImageIndex)
-        setMemeImage(memesData.data.memes[randomImageIndex].url)
-    }
+        // memesData.data.memes[randomImageIndex].url)
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme, 
+                randomImage: allMemeImages.data.memes[randomImageIndex].url
+            }
+        })
+    }   
 
     return (
         <div>
@@ -20,8 +49,11 @@ function Meme(){
             </div>
             <button onClick={getMemeImage}>Get a new meme image 🖼</button>
         </div>
-        {/* <img className="meme--image" src={memesData.data.memes[randomImageIndex].url}/> */}
-        <img className="meme--image" src={memeImage}/>
+        <div className="meme--box">
+            <img className="meme--image" src={meme.randomImage}/>
+            <h2 className="meme--text top">top ttext one top</h2>
+            <h2 className="meme--text bottom">bottom one bottom</h2>
+        </div>
         </div>
         )
         
